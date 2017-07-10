dd71cc94edd75c9b9fe8cdfa53033bde0cbfb2d2
Mark <stevens.mark.a@gmail.com>
2017-07-10 02:26:43 +0000

    New code will rotate both axes

--- test_cup_dispenser.py
+++ test_cup_dispenser.py
@@ -3,7 +3,40 @@ import time
 
 import arduino
 
-uno = arduino.Arduino("tty.usb", baud=9600)
+uno = arduino.Arduino("ttyACM", baud=9600)
+
+
+UNCONNECTED_1 = 8
+UNCONNECTED_2 = 9
+UNCONNECTED_3 = 12
+ICE_STEPS = 6
+
+class Motor(object):
+  def __init__(self, uno, dir_pin, step_pin, trigger_neg, trigger_pos, home_dir, home_offset):
+    self.uno = uno
+    self.dir_pin = dir_pin
+    self.step_pin = step_pin
+    self.trigger_neg = trigger_neg
+    self.trigger_pos = trigger_pos
+    self.home_dir = home_dir
+    self.home_offset = home_offset
+
+  def Move(self, forward, steps):
+    self.uno.Move(stepper_dir_pin=self.dir_pin,
+                  stepper_pulse_pin=self.step_pin,
+                  negative_trigger_pin=self.trigger_neg,
+                  positive_trigger_pin=self.trigger_pos,
+                  done_pin=UNCONNECTED_1,
+                  forward=forward,
+                  steps=steps,
+                  final_wait=final_wait,
+                  max_wait=max_wait,
+                  temp_pin=UNCONNECTED_2,
+                  temp_pin_threshold=ICE_STEPS)
+
+  def Calibrate(self):
+    self.Move(self.home_dir, 1500)
+    self.Move(not self.home_dir, self.home_offset)
 
 time.sleep(2)
 print "Created arduinio"
@@ -16,41 +49,35 @@ print("motor")
 #STEP_PIN = 11
 #DIR_PIN = 3
 #STEP_PIN = 2
-# Vertical
-DIR_PIN = 4
-STEP_PIN = 5
-TRIGGER_NEG = 7
-TRIGGER_POS = 12  # Should not be in use
-# Horizontal
-DIR_PIN = 3
-STEP_PIN = 2
-TRIGGER_POS = 6
-TRIGGER_NEG = 12  # Should not be in use
-# UNCONNECTED
-STEPPER_DONE = 8
-ICE_PIN = 7
-ICE_STEPS = 6
+horizontal_motor = Motor(uno,
+                         dir_pin=3,
+                         step_pin=2,
+                         trigger_pos=6,
+                         trigger_neg=UNCONNECTED_3,
+                         home_dir=1,
+                         home_offset=500)
+vertical_motor = Motor(uno,
+                       dir_pin=4,
+                       step_pin=5,
+                       trigger_pos=UNCONNECTED_3,
+                       trigger_neg=7,
+                       home_dir=0,
+                       home_offset=200)
 
-forward = 0
-steps = 200
+forward = 1
+steps = 100
 final_wait = 4000
 max_wait = 4000
+for motor in [horizontal_motor, vertical_motor]:
+  motor.Calibrate()
+  time.sleep(1)
 for i in range(6):
   print 'steps =', steps
   adjusted_steps = steps
   if not forward:
     adjusted_steps *= 2
-  uno.Move(stepper_dir_pin=DIR_PIN,
-           stepper_pulse_pin=STEP_PIN,
-           negative_trigger_pin=TRIGGER_NEG,
-           positive_trigger_pin=TRIGGER_POS,
-           done_pin=STEPPER_DONE,
-           forward=forward,
-           steps=adjusted_steps,
-           final_wait=final_wait,
-           max_wait=max_wait,
-           temp_pin=ICE_PIN,
-           temp_pin_threshold=ICE_STEPS)
+  for motor in [horizontal_motor, vertical_motor]:
+    motor.Move(forward, 200)
   forward = not forward
   time.sleep(2)
 
