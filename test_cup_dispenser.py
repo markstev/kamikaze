54602119dab89aaec8e64ec7014be0a9ebdf6abf
Mark Stevens <stevens.mark.a@gmail.com>
2017-07-09 17:19:03 -0700

    Sets up pins for the new kamikaze configuration.

--- test_cup_dispenser.py
+++ test_cup_dispenser.py
@@ -3,7 +3,7 @@ import time
 
 import arduino
 
-uno = arduino.Arduino("ttyACM1", baud=9600)
+uno = arduino.Arduino("tty.usb", baud=9600)
 
 time.sleep(2)
 print "Created arduinio"
@@ -16,41 +16,42 @@ print("motor")
 #STEP_PIN = 11
 #DIR_PIN = 3
 #STEP_PIN = 2
+# Vertical
 DIR_PIN = 4
 STEP_PIN = 5
+TRIGGER_NEG = 7
+TRIGGER_POS = 12  # Should not be in use
+# Horizontal
+DIR_PIN = 3
+STEP_PIN = 2
+TRIGGER_POS = 6
+TRIGGER_NEG = 12  # Should not be in use
 # UNCONNECTED
-TRIGGER_NEG = 6
-TRIGGER_POS = 2  # Should not be in use
 STEPPER_DONE = 8
 ICE_PIN = 7
 ICE_STEPS = 6
 
-forward = 1
+forward = 0
 steps = 200
 final_wait = 4000
 max_wait = 4000
-for i in range(50):
+for i in range(6):
   print 'steps =', steps
-  for i in range(10):
-    uno.Move(stepper_dir_pin=DIR_PIN,
-             stepper_pulse_pin=STEP_PIN,
-             negative_trigger_pin=TRIGGER_NEG,
-             positive_trigger_pin=TRIGGER_POS,
-             done_pin=STEPPER_DONE,
-             forward=forward,
-             steps=steps // 10,
-             final_wait=final_wait,
-             max_wait=max_wait,
-             temp_pin=ICE_PIN,
-             temp_pin_threshold=ICE_STEPS)
-    time.sleep(.1)
-  forward = not forward
-  """
+  adjusted_steps = steps
   if not forward:
-    steps *= 2
-  else:
-    steps /= 2
-  """
+    adjusted_steps *= 2
+  uno.Move(stepper_dir_pin=DIR_PIN,
+           stepper_pulse_pin=STEP_PIN,
+           negative_trigger_pin=TRIGGER_NEG,
+           positive_trigger_pin=TRIGGER_POS,
+           done_pin=STEPPER_DONE,
+           forward=forward,
+           steps=adjusted_steps,
+           final_wait=final_wait,
+           max_wait=max_wait,
+           temp_pin=ICE_PIN,
+           temp_pin_threshold=ICE_STEPS)
+  forward = not forward
   time.sleep(2)
 
 # NOTE, may need to ground the enable pin beyond default wiring.
